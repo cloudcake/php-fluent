297becc8b81979aab2b33ab61120a724617d11d6
Stephen Lake <stephen@slvrcld.com>
2020-03-19 16:31:56 +0200

    Add produce

--- README.md
+++ README.md
@@ -22,6 +22,7 @@ constructing data is always placed inside the helper function. This is to
 provide a more consistent, readable syntax you won't forget.
 
 Example:
+
 ```php
 str('This is my string')->startsWith('This'); // true
 ```
@@ -30,7 +31,7 @@ Usage of helpers follows this syntax for all [string helpers](https://github.com
 
 # Extras
 
-### Case-insensitivity - `ignoreCasing()`
+### Case-insensitivity `ignoreCasing()`
 
 Sometimes it's useful to call helpers ignoring the casing of strings, you can do this by chaining `->ignoreCasing()` before executing the function on the string. This provides a cleaner solution than changing the casing on every string yourself.
 
@@ -39,4 +40,29 @@ Example:
 ```php
 str('tHiS is my string')->ignoreCasing()->startsWith('THIS'); // Returns true
 str('tHiS is my string')->startsWith('THIS'); // Returns false
+```
+
+### Mutliple calls `produce(3)`
+
+There may be instances where you need to execute the same thing 10 times, for example let's say you need to generate 10 UUID's, rather than calling the function 10 times, you may use `produce(10)` method.
+
+Example:
+
+```php
+str()->produce(10)->uuid();
+
+// Returns
+// Array
+// (
+//     [0] => fc91fe0a-4254-4792-9604-e5fbf223d0a7
+//     [1] => bdc0e4a1-86ca-4dba-ae95-ac4a2ad639d0
+//     [2] => 95b06669-3262-4dfa-a9c3-69fc3251f94c
+//     [3] => f0b2aec6-fb86-4beb-bdf2-74ddb25ff440
+//     [4] => 2d0c02a0-2ec8-402f-b6eb-a1ce83f4e343
+//     [5] => e0cc9098-cebf-42ec-89b2-8942930f0293
+//     [6] => 2efba194-e2c7-48c7-965d-9efc963edf06
+//     [7] => 8e010b42-1664-4272-be2f-90402010866d
+//     [8] => ec668d35-ffd4-4b59-bd11-204fd193f6e8
+//     [9] => e74a5362-e272-4af6-ba07-3d1f4352d653
+// )
 ```
--- src/FluentString.php
+++ src/FluentString.php
@@ -8,11 +8,13 @@ use Illuminate\Support\Str;
 class FluentString
 {
     private $str;
+    private $produce;
     private $ignoreCasing;
 
     public function __construct($str = null)
     {
         $this->str = $str;
+        $this->produce = 1;
         $this->ignoreCasing = false;
     }
 
@@ -23,6 +25,28 @@ class FluentString
         return $this;
     }
 
+    public function produce($count)
+    {
+        $this->produce = $count;
+
+        return $this;
+    }
+
+    private function getArgsAsLowercase($oargs)
+    {
+        $args = [];
+
+        foreach ($oargs as $arg) {
+            if (is_string($arg)) {
+                $arg = strtolower($arg);
+            }
+
+            $args[] = $arg;
+        }
+
+        return $args;
+    }
+
     public function __call($method, $oargs)
     {
         $str = $this->str;
@@ -31,20 +55,33 @@ class FluentString
         if ($str) {
             if ($this->ignoreCasing) {
                 $str = strtolower($str);
-                $args = [];
-
-                foreach ($oargs as $arg) {
-                    if (is_string($arg)) {
-                        $arg = strtolower($arg);
-                    }
-
-                    $args[] = $arg;
-                }
+                $args = $this->getArgsAsLowercase($oargs);
             }
 
             $args = Arr::prepend($args, $str);
         }
 
+        if ($this->produce === 1) {
+            return   $this->bridge($method, $args);
+        }
+
+        $results = [];
+
+        for ($i = 0; $i < $this->produce; $i++) {
+            $result = $this->bridge($method, $args);
+
+            if (method_exists($result, '__toString')) {
+                $result = $result->__toString();
+            }
+
+            $results[] = $result;
+        }
+
+        return $results;
+    }
+
+    private function bridge($method, $args)
+    {
         return Str::$method(...$args);
     }
 }
