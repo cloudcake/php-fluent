95447658d482d6c17def083a2e37e83eff503535
Stephen Lake <stephen@slvrcld.com>
2020-03-19 16:06:58 +0200

    Add ignoreCasing

--- src/FluentString.php
+++ src/FluentString.php
@@ -8,16 +8,41 @@ use Illuminate\Support\Str;
 class FluentString
 {
     private $str;
+    private $ignoreCasing;
 
     public function __construct($str = null)
     {
         $this->str = $str;
+        $this->ignoreCasing = false;
     }
 
-    public function __call($method, $args)
+    public function ignoreCasing()
     {
-        if ($this->str) {
-            $args = Arr::prepend($args, $this->str);
+        $this->ignoreCasing = true;
+
+        return $this;
+    }
+
+    public function __call($method, $oargs)
+    {
+        $str = $this->str;
+        $args = $oargs;
+
+        if ($str) {
+            if ($this->ignoreCasing) {
+                $str = strtolower($str);
+                $args = [];
+
+                foreach ($oargs as $arg) {
+                    if (is_string($arg)) {
+                        $arg = strtolower($arg);
+                    }
+
+                    $args[] = $arg;
+                }
+            }
+
+            $args = Arr::prepend($args, $str);
         }
 
         return Str::$method(...$args);
